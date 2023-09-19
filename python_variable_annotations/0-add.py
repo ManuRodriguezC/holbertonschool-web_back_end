dda1511274998ca175bf963f1614ee0e82d9529f
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-19 14:28:11 -0500

    styles and document

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,5 +3,11 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+    add function give two params:
+    a: The first number which is a float
+    b: The second number which is a float
+    Return a float it is add of a and b
+    """
     sum = a + b
     return sum
