3fc2bd09b003ffbb2077ddac6929d15a4238fe88
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-19 14:39:30 -0500

    Changes some erros

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -3,7 +3,7 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     This funcion receives two arguments
     k: Is a string
