7d11f55d727a51e8df4035b77169cb10cf491135
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-26 17:14:37 -0500

    Async implement in the async functions

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -15,4 +15,5 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         number = await wait_random(max_delay)
         lists.append(number)
         count += 1
+    lists.sort()
     return lists
