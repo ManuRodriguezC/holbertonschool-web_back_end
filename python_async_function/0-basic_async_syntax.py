ab4b359c0471cbeb7d5c18dea8aecc50c6db9921
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-26 15:55:39 -0500

    Learning about asyncio library

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,14 @@
 #!/usr/bin/env python3
+"""This module create a function that wait delay time"""
 import asyncio
 import random
 
 
-async def wait_random(max_delay:int = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    This function is async and give one param
+    max_delay is a time delay
+    """
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
