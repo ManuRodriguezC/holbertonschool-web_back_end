331e237764c477574e8e7c1915bff96d4a850710
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-26 18:20:59 -0500

    Cron the time of the execute

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -6,10 +6,9 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    start = time.process_time()
+    start: float = time.process_time()
     asyncio.run(wait_n(n, max_delay))
-    end = time.process_time()
-    total = start - end
+    end: float = time.process_time()
+
+    total: float = start - end
     return total / n
-
-
