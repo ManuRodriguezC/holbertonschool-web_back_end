4b6f56b3dcc5b1720e4ef3165afa1abd6fe7c11c
ManuRodriguezC <estebanclimb@gmail.com>
2023-09-28 18:04:43 -0500

    Start the caching proyect

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This module inherits from basecaching"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    This class creates caches by inheriting
+    the cahe_data dictionary in which the cahe
+    data will be saved using a key and a value,
+    so that it can be accessed at any time.
+    """
+    
+    def put(self, key, item):
+        """
+        This method add dates a dictionaty caching
+        through key and value.
+        """
+        if key != None and item != None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        This method return the value of the one date or key
+        is this date not exist return None through management errors.
+        """
+        try:
+            return self.cache_data[key]
+        except:
+            KeyError(None)
