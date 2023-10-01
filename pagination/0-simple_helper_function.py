4e6e542df0ac5eb870e4ccc0918a6bb41a087913
ManuRodriguezC <estebanclimb@gmail.com>
2023-10-01 11:27:57 -0500

    Pagination

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,9 @@
+#!/user/bin/nev python3
+""""""
+
+def index_range(page, page_size):
+    """"""
+    index = page - 1
+    start_index = index * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
