ecabe98805708f675368912b711f30babaa4d589
ManuRodriguezC <estebanclimb@gmail.com>
2023-10-05 08:54:28 -0500

    Task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,35 @@
 """Filtered logger"""
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """
+        This method init the class, add give one parameter
+        fields, this contein the dates that you need to hide.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        This method format the loggin dates.
+        """
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION,
+                                  record.msg,
+                                  self.SEPARATOR)
+        return super().format(record)
 
 
 def filter_datum(fields: List[str],
