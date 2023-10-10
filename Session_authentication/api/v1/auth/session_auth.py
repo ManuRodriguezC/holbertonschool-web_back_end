0bd06644a1c1f796e287234ddd7956dba77a977c
ManuRodriguezC <estebanclimb@gmail.com>
2023-10-10 16:04:50 -0500

    Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 """Module Session Auth"""
 from api.v1.auth.auth import Auth
+import base64
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """ Class Session auth """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+            This methos create a session auth
+            Whit the user_id, create a key witj uuid4
+            and save the values in the emply file
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        key: str = str(uuid4())
+        self.user_id_by_session_id[key] = user_id
+
+        return key
