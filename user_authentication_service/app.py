33cd08f3605ea786fc10505fb2470584c50975a2
ManuRodriguezC <estebanclimb@gmail.com>
2023-10-15 07:28:44 -0500

    Create  Basic Flask

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 """Route module for the API"""
-from flask import Flask, jsonify, Response
+import flask
 from typing import Tuple
 
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 app.register_blueprint()
 
 
 @app.route('/', methods=['GET'], strict_slaches=False)
-def welcome() -> Tuple[Response, int]:
+def welcome() -> Tuple[flask.Response, int]:
     """ Welcome route """
-    return jsonify({"message": "Bienvenue"}), 200
+    return flask.jsonify({"message": "Bienvenue"}), 200
 
 
 if __name__ == "__main__":
