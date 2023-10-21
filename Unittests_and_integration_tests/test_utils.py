110b7d113b3b1798354a57e809a63afbd4035852
ManuRodriguezC <estebanclimb@gmail.com>
2023-10-21 10:23:51 -0500

    Add test access

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ This module contention the tests with unittest"""
 from unittest import TestCase
-import parameterized
+from parameterized import parameterized
 from typing import Mapping
 from utils import access_nested_map
 
@@ -9,9 +9,12 @@ from utils import access_nested_map
 class TestAccessNestedMap(TestCase):
     """ This class check and create test of the utils functions """
 
-    @parameterized.expand()
-    def test_access_nested_map(self):
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
         """ This function check an test the access nested map is correct"""
-        nested = {"a": {"h": {"t": 11}}}
-        path = ["a", "h", "t", "z"]
-        self.assertEqual(Mapping, access_nested_map(nested, path))
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
