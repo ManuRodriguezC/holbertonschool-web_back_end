32bd86cf915a8dd07fee0346bec0e79067920ba2
ManuRodriguezC <estebanclimb@gmail.com>
2023-10-21 10:15:29 -0500

    Unittests and Integrations tests

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,16 @@
 #!/usr/bin/env python3
 """ This module contention the tests with unittest"""
-import unittest
+from unittest import TestCase
 import parameterized
 from typing import Mapping
 from utils import access_nested_map
 
 
-class TestAccessNetedMap(unittest.TestCase):
+class TestAccessNestedMap(TestCase):
     """ This class check and create test of the utils functions """
 
-    @parameterized.expand(
-        nested_map={"a": 1}, path=("a",),
-        nested_map={"a": {"b": 2}}, path=("a",),
-        nested_map={"a": {"b": 2}}, path=("a", "b")
-    )
     def test_access_nested_map(self):
         """ This function check an test the access nested map is correct"""
         nested = {"a": {"h": {"t": 11}}}
-        path = ["a", "h", "t", "z"]
+        path = ["a", "h", "t"]
         self.assertEqual(Mapping, access_nested_map(nested, path))
