219875755bdad124ff2de3db2d9e491a5ae1fec0
ManuRodriguezC <estebanclimb@gmail.com>
2023-10-24 20:19:58 -0500

    Babel use

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -4,18 +4,22 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-app = Flask(__name__)
-bable = Babel(app)
-app.config["en", "fr"]
-
 class Config():
     LANGUAGES = ["en" , "fr"]
+    BABEL_DEFAULT_LOCATE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+bable = Babel(app, Config.BABEL_DEFAULT_LOCATE, Config.BABEL_DEFAULT_TIMEZONE)
+
 
 
 @app.route("/")
 def index():
     """ This funtion return the index web """
-    return render_template("0-index.html")
+    return render_template("0-index.html"), 200
 
 
 if __name__ == "__main__":
