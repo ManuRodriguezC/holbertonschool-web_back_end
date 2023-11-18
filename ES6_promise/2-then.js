fbb20c0024577698532bf3ceef11729522ac400c
ManuRodriguezC <estebanclimb@gmail.com>
2023-11-18 10:57:36 -0500

    check

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-    promise
-        .then(() => {
-            console.log('Got a response from the API');
-            return {
-                status: 200,
-                body: 'success'
-            };
-        })
-        .catch(() => {
-            return new Error();
-        });
+  promise
+      .then((result) => {
+          console.log('Got a response from the API');
+          return result;
+      })
+      .catch((error) => new Error());
+      return promise
 }
