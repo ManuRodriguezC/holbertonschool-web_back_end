ba5794dfceb5731c0644d7a8034bd639e9b6cfe0
ManuRodriguezC <estebanclimb@gmail.com>
2023-11-19 12:05:32 -0500

    static methos and class call class

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,9 +1,15 @@
-import Currency from './3-currency.js';
+import Currency from "./3-currency";
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (typeof(amount) !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
     this._amount = amount;
-    this._currency = currency;
+    this._currency = currency;  
   }
 
   get amount() {
@@ -11,6 +17,9 @@ export default class Pricing {
   }
 
   set amount(newAmount) {
+    if (typeof(amount) !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
     this._amount = newAmount;
   }
 
@@ -19,14 +28,20 @@ export default class Pricing {
   }
 
   set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency');
+    }
     this._currency = newCurrency;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`
   }
 
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+    if (typeof(amount) !== 'number' || typeof(conversionRate) !== 'number') {
+      throw new TypeError('Amount and conversion rate must be numbers');
+    }
+    return amount * conversionRate
   }
 }
