0121dc430b4c945823132af2f82b3d50702b9e33
ManuRodriguezC <estebanclimb@gmail.com>
2023-11-19 12:54:57 -0500

    static methos and class call class

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,4 +1,4 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
@@ -23,7 +23,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
   }
 
   static convertPrice(amount, conversionRate) {
