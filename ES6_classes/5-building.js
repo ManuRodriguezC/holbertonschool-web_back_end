2f903f23e33e7cfa6248ac28fbf98e823edbccb5
ManuRodriguezC <estebanclimb@gmail.com>
2023-11-19 13:09:03 -0500

    building

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,16 @@
 export default class Building {
-    constructor(sqft) {
-        if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
-            throw new Error('Class extending Building must override evacuationWarningMessage');
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    set sqft(newSqft) {
-        this._sqft = newSqft;
-    }
+  set sqft(newSqft) {
+    this._sqft = newSqft;
+  }
 }
