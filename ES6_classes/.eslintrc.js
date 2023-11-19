e4fa9bdf1d326101ff504036f4d954157008b58d
ManuRodriguezC <estebanclimb@gmail.com>
2023-11-19 11:17:41 -0500

    set a getter methods

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,42 +1,37 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  'extends': 'eslint:recommended',
-  'overrides': [
-    {
-      'env': {
-        'node': true
-      },
-      'files': [
-        '.eslintrc.{js,cjs}'
-      ],
-      'parserOptions': {
-        'sourceType': 'script'
-      }
-    }
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module'
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  'rules': {
-    'indent': [
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
       'error',
-      2
+      'LabeledStatement',
+      'WithStatement',
     ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ]
-  }
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,31 +1,31 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
-  
+
   get name() {
-    return this._name;
+    return this.name;
   }
   
   set name(newName) {
     if (typeof(newName) !== 'string') {
       throw TypeError('Name must be a string');
     }
-    this._name = newName;
+    this.name = newName;
   }
 
   get length () {
-    return this._length;
+    return this.length;
   }
 
   set length(newlength) {
     if (typeof(newlength) !== 'number') {
       throw TypeError('Length must be a number');
     }
-    this._length = newlength;
+    this.length = newlength;
   }
 
   get students() {
@@ -36,6 +36,6 @@ export default class HolbertonCourse {
     if (typeof(newStudents) !== 'object') {
       throw TypeError('Students must be a array of Strings');
     }
-    this._students = newStudents;
+    this.students = newStudents;
   }
 }
