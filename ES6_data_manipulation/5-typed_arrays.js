366745b79a1543f20811049272952b013ab358f7
ManuRodriguezC <estebanclimb@gmail.com>
2023-11-28 17:30:15 -0500

    Arrays types

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,5 +1,9 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new DataView(new ArrayBuffer(length));
-  buffer.setInt8(position,value);
+  try {
+    buffer.setInt8(position, value);
+  } catch (error) {
+    throw new Error('Position outside range')
+  }
   return buffer;
 }
