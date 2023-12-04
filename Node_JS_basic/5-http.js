fb3e5fa482c3ec9f79934c40b6671bf770582888
ManuRodriguezC <estebanclimb@gmail.com>
2023-12-04 12:27:38 -0500

    Express complex

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -28,15 +28,15 @@ const app = http.createServer((request, response) => {
     response.end('Hello Holberton School!');
   } else if (request.url === '/students' || request.url === '/students/') {
     let data;
+    let datesStudents = 'This is the list of our students\n';
     try {
       data = fs.readFileSync(database, 'utf8');
     } catch (error) {
       response.writeHead(500);
-      response.end('Cannot load the database');
+      response.end(`${datesStudents}Cannot load the database`);
     }
 
     response.writeHead(200);
-    let datesStudents = 'This is the list of our students\n';
     datesStudents += students(data).join('\n');
     response.end(datesStudents);
   } else {
