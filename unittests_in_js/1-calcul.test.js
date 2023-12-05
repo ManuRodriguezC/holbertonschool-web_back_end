5798553ef1380237aaa86e034db65f073b433764
ManuRodriguezC <estebanclimb@gmail.com>
2023-12-05 10:17:25 -0500

    Chech combining descriptions

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -14,15 +14,15 @@ describe('calculateNumber', () => {
     assert.equal(calculateNumber('SUM', 4.5, -1.75), 3);
   });
   it('Return a - b rounded numbers when type SUBTRACT', () => {
-    assert.equal(calculateNumber('SUBTRACT', 1, 5), 4);
-    assert.equal(calculateNumber('SUBTRACT', 4, 2), -2);
-    assert.equal(calculateNumber('SUBTRACT', 1.25, 3), 2);
-    assert.equal(calculateNumber('SUBTRACT', 4.5, 2), -3);
-    assert.equal(calculateNumber('SUBTRACT', -1, 3), 4);
+    assert.equal(calculateNumber('SUBTRACT', 1, 5), -4);
+    assert.equal(calculateNumber('SUBTRACT', 4, 2), 2);
+    assert.equal(calculateNumber('SUBTRACT', 1.25, 3), -2);
+    assert.equal(calculateNumber('SUBTRACT', 4.5, 2), 3);
+    assert.equal(calculateNumber('SUBTRACT', -1, 3), -4);
     assert.equal(calculateNumber('SUBTRACT', -5, -5), 0);
     assert.equal(calculateNumber('SUBTRACT', -2.5, -2.5), 0);
-    assert.equal(calculateNumber('SUBTRACT', -2.25, 2.25), 4);
-    assert.equal(calculateNumber('SUBTRACT', 4.5, -1.75), -7);
+    assert.equal(calculateNumber('SUBTRACT', -2.25, 2.25), -4);
+    assert.equal(calculateNumber('SUBTRACT', 4.5, -1.75), 7);
   });
   it('Return a / b when b is letter that 0 and type is DIVIDE', () => {
     assert.equal(calculateNumber('DIVIDE', 1, 5), 0.2);
